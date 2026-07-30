8f4239aa7c7de5d57d04a2fb33ddc29588b216ab
VinisHub <vineethn24bcs0178@iiitkottayam.ac.in>
2026-07-30 15:58:34 +0530

    Solve fibonacci-number [cpp]

--- submissions/fibonacci-number/solution.cpp
+++ submissions/fibonacci-number/solution.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
-    int fib(int n) {
+    int solve(int n,vector<int> &dp){
         if(n == 0) return 0;
         if(n == 1) return 1;
-        return fib(n-1) + fib(n-2);
+
+        
+        if(dp[n] != -1) return dp[n];
+       
+
+        return dp[n]=fib(n-1) + fib(n-2);
+    }
+    int fib(int n) {
+        vector<int> dp(n+1, -1);
+        return solve(n, dp);
     }
 };
