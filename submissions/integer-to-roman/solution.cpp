72ecec11c131fcac4b1fcf67c74ce7f7df655d8c
VinisHub <vineethn24bcs0178@iiitkottayam.ac.in>
2026-08-05 17:13:10 +0530

    Solve integer-to-roman [cpp]

--- submissions/integer-to-roman/solution.cpp
+++ submissions/integer-to-roman/solution.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<int> values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        vector<string> symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        unordered_map<int, string> mp = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+        };
+        vector<int> order = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         
         string result = "";
-        for (int i = 0; i < values.size(); i++) {
-            while (num >= values[i]) {
-                num -= values[i];
-                result += symbols[i];
+        for (int val : order) {
+            while (num >= val) {
+                num -= val;
+                result += mp[val];   // O(1) average lookup
             }
         }
         return result;
