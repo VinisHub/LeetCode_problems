69b197a6b4807842674c3808df9ae68cea875f35
VinisHub <vineethn24bcs0178@iiitkottayam.ac.in>
2026-08-08 09:14:25 +0530

    Solve check-if-array-is-sorted-and-rotated [cpp]

--- submissions/check-if-array-is-sorted-and-rotated/solution.cpp
+++ submissions/check-if-array-is-sorted-and-rotated/solution.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        vector<int> b, sorted = nums;
-        int x;
-        sort(sorted.begin(), sorted.end());
+        int count = 0;
         for(int i=0;i<nums.size();i++){
-            x = i;
-            for(int j=0;j<nums.size();j++){
-                b.push_back(nums[(j+x) % nums.size()]);
-            }
-            if(sorted == b) return true;
-            b.clear();
+            if(nums[i] > nums[(i+1) % nums.size()]) count++;
         }
-        return false;
+        return count <= 1;
     }
 };
