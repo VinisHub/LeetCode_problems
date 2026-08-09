188ee6ab79ddf92e106e383e93ef1160221ffa5e
VinisHub <vineethn24bcs0178@iiitkottayam.ac.in>
2026-08-09 16:45:43 +0530

    Solve two-sum [cpp]

--- submissions/two-sum/solution.cpp
+++ submissions/two-sum/solution.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
-
-        for (int i = 0; i < nums.size(); i++) {
-            int complement = target - nums[i];
-
-            if (mp.find(complement) != mp.end()) {
-                return {mp[complement], i};
+        vector<int> ans;
+        int temp ,j = 1;
+        for(int i=0;i<nums.size()-1;i++){
+            temp = target - nums[i];
+            j = i + 1;
+            while(j < nums.size()){
+                if(nums[j]  == temp) {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                }
+                j++;
             }
-
-            mp[nums[i]] = i;
         }
-
-        return {};
+        return ans;
     }
 };
