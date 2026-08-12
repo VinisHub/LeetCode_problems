cf14404963f7eb07110ef84aabe836b263f360fd
VinisHub <vineethn24bcs0178@iiitkottayam.ac.in>
2026-08-12 09:23:36 +0530

    Solve move-zeroes [cpp]

--- submissions/move-zeroes/solution.cpp
+++ submissions/move-zeroes/solution.cpp
@@ -3,21 +3,14 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
         int temp;
-        vector<int> mpp(n, 0);
-        for(int i=0;i<n;i++){
+        for(int i = n-2;i>=0;i--){
             if(nums[i] == 0){
-                mpp[i] = 1;
+                for(int j=i;j<n-1;j++){
+                    temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
             }
         }
-        for(int j=n-1;j>=0;j--){
-            if(mpp[j] == 1){
-                int p = j;
-                while(p<n-1){
-                nums[p] = nums[p+1];
-                p++;
-                }
-                nums[p] = 0;
-            }
-        }    
     }
 };
