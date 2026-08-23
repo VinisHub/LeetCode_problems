94d5f85186ae8929cf1bea2cc0aae487ef4ab753
VinisHub <vineethn24bcs0178@iiitkottayam.ac.in>
2026-08-24 05:25:39 +0530

    Solve subarray-sum-equals-k [cpp]

---
+++ submissions/subarray-sum-equals-k/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        int preSum = 0, count = 0;
+        mp[0] = 1;
+        for(int i=0;i<nums.size();i++){
+            preSum += nums[i];
+            int remove = preSum - k;
+            count += mp[remove];
+            mp[preSum] += 1;
+        }
+        return count;
+    }
+};
