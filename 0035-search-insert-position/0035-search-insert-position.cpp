b134eb3752d733a5bc3daeb22f66a7eb145bebc3
VINEETH MN <vineethn24bcs0178@iiitkottayam.ac.in>
2026-09-19 11:28:18 +0530

    0035-SEARCH-INSERT-POSITION Stats: Time: 0 ms (100%), Space: 13.7 MB (43.05%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low=0;
+        int high = nums.size() - 1;
+        int mid;
+        if(target < nums[low]) return low;
+        if(target > nums[high]) return nums.size();
+        while(low <= high){
+            mid = low + (high - low)/2;
+            if(target == nums[mid]) return mid;
+            else if(target > nums[mid]) low = mid+1;
+            else high = mid-1;
+        }
+        return low;
+    }
+};
